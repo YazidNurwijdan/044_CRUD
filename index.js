6e148807f1fa634aa4a5b9eec3ba395d72f5a6e5
YazidNurwijdan <ynurwijdan@gmail.com>
2025-10-20 12:47:05 +0700

    setup express.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-git branch -M main
+const express = require("express");
+let mysql = require("mysql2");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
